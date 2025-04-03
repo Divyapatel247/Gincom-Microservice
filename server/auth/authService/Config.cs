9da23dcc14423e2cc081bb71a93ab263b480dd18
khushibansal13 <khushibansal1302@gmail.com>
2025-04-03 13:24:41 +0530

    Config updated to handle username and role

--- server/auth/authService/Config.cs
+++ server/auth/authService/Config.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources =>
+   public static IEnumerable<IdentityResource> IdentityResources =>
         new List<IdentityResource>
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-            new IdentityResources.Email()
+            new IdentityResources.Email(),
+            new IdentityResource("roles", "User roles", new[] { "role" }) 
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
         {
-            new ApiScope("api.read", "Read access to API"),
-            new ApiScope("api.write", "Write access to API")
+            new ApiScope("api.read", "Read access to API", new[] { "email", "username", "role" }),
+            new ApiScope("api.write", "Write access to API", new[] { "email", "username", "role" })
         };
 
     public static IEnumerable<Client> Clients =>
@@ -26,8 +27,12 @@ public static class Config
                 ClientId = "client",
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                 ClientSecrets = { new Secret("secret".Sha256()) },
-                AllowedScopes = { "api.read", "api.write", "openid", "profile", "email" },
-                AllowOfflineAccess = true
+                
+                AllowedScopes = { "api.read", "api.write", "openid", "profile", "email", "roles" },
+
+                AllowOfflineAccess = true, 
+                
+                AlwaysIncludeUserClaimsInIdToken = true 
             }
         };
 }
