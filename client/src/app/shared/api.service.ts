7d6a4e28ab091d0198cdd0bd9982d047a70b50d4
DivyPatel247 <medivyapatel27@gmail.com>
2025-04-24 12:32:19 +0530

    Revert "order detail page added"
    This reverts commit 8438ef3e7e5f6bc9802f04b5c80b7bbc11f4c6d6.

--- client/src/app/shared/api.service.ts
+++ client/src/app/shared/api.service.ts
@@ -261,20 +261,12 @@ export class ApiService {
     return this.http.post<BasketResponse>(`${this.apiUrl}/api/orders/${userId}/cart/items/bulk`, request, {headers});
   }
 
-  // getOrdersByProductId(productId:number): Observable<> {
-  //   const token = localStorage.getItem('access_token');
-  //   const headers = new HttpHeaders({
-  //      Authorization: `Bearer ${token}`
-  //    });
-  //   return this.http.get<>(`${this.apiUrl}/api/orders/product/${productId}`, {headers});
-  // }
-
-  getOrderByOrderId(orderId:number): Observable<Order> {
+  getOrdersByProductId(productId:number): Observable<BasketResponse> {
     const token = localStorage.getItem('access_token');
     const headers = new HttpHeaders({
        Authorization: `Bearer ${token}`
      });
-    return this.http.get<Order>(`${this.apiUrl}/api/orders/${orderId}`, {headers});
+    return this.http.get<BasketResponse>(`${this.apiUrl}/api/orders/product/${productId}`, {headers});
   }
 
 }
